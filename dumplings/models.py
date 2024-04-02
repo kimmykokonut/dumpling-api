453b96dc9714b90b077eef2ff704080583022ac0
Kim Robinson <kimmykokonut@gmail.com>
2024-04-02 15:50:47 -0700

    successfully add Django Rest Auth Tokens and endpoints functional for login, signup, testtoken

--- dumplings/models.py
+++ dumplings/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 # so django knows models/db
+# from pygments.lexers import get_lexer_by_name
+# from pygments.formatters.html import HtmlFormatter
+# from pygments import highlight
+
 class Dumpling(models.Model): 
   # no need add DumplingId
   name = models.CharField(max_length=255)
   description = models.CharField(max_length=500)
   origin = models.ForeignKey('Origin', on_delete=models.SET_NULL, null=True, default=1, related_name='dumplings')
+  # add user info for auth
+  # owner = models.ForeignKey('auth.User', related_name='dumplings', on_delete=models.CASCADE)
+  # highlighted = models.TextField()
   # create method on dumpling obj so in admin it is it's name vs dumpling object 1
   def __str__(self):
     return self.name + ': ' + self.description
+  # for auth?
+  # def save(self, *args, **kwargs):
+
   
 class Origin(models.Model):
   country = models.CharField(max_length=60)
--- dumplings/views.py
+++ dumplings/views.py
@@ -1,10 +1,46 @@
 from django.http import JsonResponse
 from .models import Dumpling, Tag, Origin
-from .serializers import DumplingSerializer, TagSerializer, OriginSerializer
+from .serializers import DumplingSerializer, TagSerializer, OriginSerializer, UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
+# AUTH
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+#for AUTH
+@api_view(['POST'])
+def login(request):
+  user = get_object_or_404(User, username=request.data['username'])
+  if not user.check_password(request.data['password']):
+    return Response({"detail": "Not Found."}, status=status.HTTP_404_NOT_FOUND)
+  token, created = Token.objects.get_or_create(user=user)
+  serializer = UserSerializer(instance=user)
+  return Response({"token": token.key, "user": serializer.data})
+
+@api_view(['POST'])
+def signup(request):
+  serializer = UserSerializer(data=request.data)
+  if serializer.is_valid():
+    user = serializer.save()
+    # user = User.objects.get(username=request.data['username'])
+    #hash pw. so og not stored
+    user.set_password(request.data['password'])
+    user.save()
+    token = Token.objects.create(user=user)
+    return Response({"token": token.key, "user": serializer.data})
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+  return Response("passed for {}".format(request.user.email))
 
 @api_view(['GET', 'POST'])
 def dumpling_list(request, format=None):
